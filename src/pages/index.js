7a43e0ebb9013889afeb74103dc0ff69cf447e82
Surya Dantuluri <surya.mvhs@gmail.com>
2018-08-28 18:53:12 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hey there, I'm Surya Dantuluri</h1>
+    <p>I've attended dozens of hackathons in the past. I'm excited to attend JAMStack Hackathon as well.</p>
+    <p>I'm always looking to network with people (especially at hackathons).</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
